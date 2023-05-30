2554314b4e0a8536f7f24344405437ee7a0331ec
corvusdeinanis <25402014+corvusdeinanis@users.noreply.github.com>
2023-05-30 20:53:47 +0530

    Update eleventyComputed.js

--- .app/_data/eleventyComputed.js
+++ .app/_data/eleventyComputed.js
@@ -1,7 +1,5 @@
 const { userComputed } = require("../userUtils");
 
 module.exports = {
-  graph: (data) => getGraph(data),
-  filetree: (data) => getFileTree(data),
   userComputed: (data) => userComputed(data),
 };
