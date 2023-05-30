aa86d50657320abf6b33bbd826add6c1e012985d
corvusdeinanis <25402014+corvusdeinanis@users.noreply.github.com>
2023-05-31 01:38:33 +0530

    Update wikilinks.js

--- .app/core/wikilinks.js
+++ .app/core/wikilinks.js
@@ -1,100 +1,98 @@
-const html = require("nanohtml");
+const wikiLinkRegex = /\[\[(.*?\|.*?)\]\]/g;
+const internalLinkRegex = /href="\/(.*?)"/g;
 
-const regex = /^\[\[([^|\]\n]+)(\|([^\]\n]+))?\]\]/;
+function caselessCompare(a, b) {
+  return a.toLowerCase() === b.toLowerCase();
+}
 
-module.exports = (md, options) => {
-  md.inline.ruler.after("link", "wikilink", inline);
-  md.renderer.rules["wikilink"] = render;
+function extractLinks(content) {
+  return [
+    ...(content.match(wikiLinkRegex) || []).map(
+      (link) =>
+        link
+          .slice(2, -2)
+          .split("|")[0]
+          .replace(/.(md|markdown)\s?$/i, "")
+          .replace("\\", "")
+          .trim()
+          .split("#")[0]
+    ),
+    ...(content.match(internalLinkRegex) || []).map(
+      (link) =>
+        link
+          .slice(6, -1)
+          .split("|")[0]
+          .replace(/.(md|markdown)\s?$/i, "")
+          .replace("\\", "")
+          .trim()
+          .split("#")[0]
+    ),
+  ];
+}
 
-  function inline(state, silent) {
-    const src = state.src.substring(state.pos);
-    const match = regex.exec(src);
-
-    if (!match) return false;
-
-    state.pos += match[0].length;
-
-    if (!silent) {
-      const token = state.push("wikilink", "", 0);
-      token.meta = { match };
+function getGraph(data) {
+  let nodes = {};
+  let links = [];
+  let stemURLs = {};
+  let homeAlias = "/";
+  (data.collections.note || []).forEach((v, idx) => {
+    let fpath = v.filePathStem.replace("/notes/", "");
+    let parts = fpath.split("/");
+    let group = "none";
+    if (parts.length >= 3) {
+      group = parts[parts.length - 2];
     }
-
-    return true;
-  }
-
-  function render(tokens, index, _options, env) {
-    const match = tokens[index].meta.match;
-    const { href, label } = processMatch(match, env);
-    return html`<a href="${href}">${label}</a>`;
-  }
-
-  function processMatch(match, env) {
-    const [, path, , text] = match;
-    const { file, hash } = processPath(path);
-    const page = findMatchingPage(file, env);
-    const label = createLabel(text, file, hash, page);
-
-    let href = page ? page.url : options.slugify(`/${pageName}`);
-    hash && (href += `#${options.slugify(hash)}`);
-
-    return { href, label };
-  }
-
-  function createLabel(text, file, hash, page) {
-    if (text) return text;
-
-    let label = file;
-
-    switch (options.autoLabel) {
-      case "title":
-        label = page.data.title || page.fileSlug || file;
-        break;
-      case "fileSlug":
-        label = page.fileSlug || file;
-        break;
-      case "ref":
-      default:
-        break;
+    nodes[v.url] = {
+      id: idx,
+      title: v.data.title || v.fileSlug,
+      url: v.url,
+      group,
+      home:
+        v.data["dg-home"] ||
+        (v.data.tags && v.data.tags.indexOf("gardenEntry") > -1) ||
+        false,
+      outBound: extractLinks(v.template.frontMatter.content),
+      neighbors: new Set(),
+      backLinks: new Set(),
+      noteIcon: v.data.noteIcon || process.env.NOTE_ICON_DEFAULT,
+      hide: v.data.hideInGraph || false,
+    };
+    stemURLs[fpath] = v.url;
+    if (
+      v.data["dg-home"] ||
+      (v.data.tags && v.data.tags.indexOf("gardenEntry") > -1)
+    ) {
+      homeAlias = v.url;
     }
-
-    if (hash) {
-      switch (options.anchorLabel) {
-        case "arrow":
-          label += ` → ${hash}`;
-          break;
-        case "parentheses":
-          label += ` (${hash})`;
-          break;
-        case "hash":
-          label += `#${hash}`;
-          break;
-        case "none":
-        default:
-          break;
+  });
+  Object.values(nodes).forEach((node) => {
+    let outBound = new Set();
+    node.outBound.forEach((olink) => {
+      let link = (stemURLs[olink] || olink).split("#")[0];
+      outBound.add(link);
+    });
+    node.outBound = Array.from(outBound);
+    node.outBound.forEach((link) => {
+      let n = nodes[link];
+      if (n) {
+        n.neighbors.add(node.url);
+        n.backLinks.add(node.url);
+        node.neighbors.add(n.url);
+        links.push({ source: node.id, target: n.id });
       }
-    }
+    });
+  });
+  Object.keys(nodes).map((k) => {
+    nodes[k].neighbors = Array.from(nodes[k].neighbors);
+    nodes[k].backLinks = Array.from(nodes[k].backLinks);
+    nodes[k].size = nodes[k].neighbors.length;
+  });
+  return {
+    homeAlias,
+    nodes,
+    links,
+  };
+}
 
-    return label;
-  }
-
-  function findMatchingPage(pageName, env) {
-    pageName = pageName.toLowerCase();
-
-    const pages = env.collections[options.collections];
-    const match =
-      pages.find((f) => normalizePath(f.filePathStem) === pageName) ||
-      pages.find((f) => normalizePath(f.filePathStem).endsWith(`/${pageName}`));
-
-    return match;
-  }
-
-  function processPath(path) {
-    const [file, ...hashes] = path.split("#");
-    const hash = hashes.join("#");
-    return { file, hash };
-  }
-
-  function normalizePath(path) {
-    return path.replace(/^\//, "").toLowerCase();
-  }
-};
+exports.wikiLinkRegex = wikiLinkRegex;
+exports.internalLinkRegex = internalLinkRegex;
